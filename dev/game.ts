03de0b56f331d92e3773c265c2e9021fa3e73684
Karim Bakkes <Karim@MacBook-Pro-van-Karim.local>
2017-05-23 11:03:06 +0200

    added comments

--- dev/game.ts
+++ dev/game.ts
@@ -7,7 +7,7 @@ class Game {
     private cat : Cat;
     private rings: Array<Ring> = new Array<Ring>();
     private utils:Utils;
-   
+
     public static instance:Game;
     
     private score: number = 0;
@@ -16,6 +16,7 @@ x
         this.cat = new Cat(5,200);
         this.utils = new Utils();
         
+        // maakt meerdere ringen aan en zet deze in de array. 
         for (let i=0; i<12; i+=1) {
         let x = Math.floor(Math.random() * 900) + 100
         let y = Math.floor(Math.random() * 900) + 100
@@ -25,6 +26,7 @@ x
         requestAnimationFrame(() => this.gameLoop());
     }
 
+        // als er geen game instance is word deze aangemaakt
         public static getInstance(){
         if(Game.instance == null){
             Game.instance = new Game();
